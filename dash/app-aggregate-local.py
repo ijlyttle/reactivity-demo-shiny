0c7cd0213c1e82e761bbec6f96c6dc9a962e3ce3
Ian Lyttle <ian.lyttle@se.com>
2021-12-14 19:55:56 -0600

    make a little more progress with dash

--- dash/app-aggregate-local.py
+++ dash/app-aggregate-local.py
@@ -4,26 +4,66 @@
 import dash
 from dash import dash_table
 from dash import html
+from dash import dcc
 import pandas as pd
 from palmerpenguins import load_penguins
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__)
+
+app = dash.Dash(
+    __name__, 
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = load_penguins()
 
-app.layout = html.Div(children=[
-
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-        page_size=10,
-        sort_action='native'
-    )
-
-])
+app.layout = html.Div(
+    className='container-fluid',
+    children=[
+        html.H2('Aggregator'),
+        html.Div(
+            className='row',
+            children =[
+                html.Div(
+                    className='col-sm-4',
+                    children=[
+                        dbc.Card([
+                            dbc.CardHeader('Input data'),
+                            dbc.CardBody([
+                                  dcc.Upload(dbc.Button('Upload CSV File'))
+                            ])
+                        ]),
+                        dbc.Card([
+                            dbc.CardHeader('Aggregation'),
+                            dbc.CardBody()
+                        ]),
+                        dbc.Card([
+                            dbc.CardHeader('Aggregated data'),
+                            dbc.CardBody()
+                        ])
+                    ]
+                ),
+                html.Div(
+                    className='col-sm-8',
+                    children=[
+                        html.H3('Input data'),
+                        dash_table.DataTable(
+                            id='table',
+                            columns=[{'name': i, 'id': i} for i in df.columns],
+                            data=df.to_dict('records'),
+                            page_size=10,
+                            sort_action='native'
+                        ),
+                        html.Hr(),
+                        html.H3('Aggregated data')
+                    ]
+                )
+            ]
+        )
+    ]
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
