bfb31810b765a1125812d73b8a3d69f5bdab4623
Ian Lyttle <ijlyttle@users.noreply.github.com>
2022-02-13 12:53:19 -0600

    use where()

--- R/aggregate-local.R
+++ R/aggregate-local.R
@@ -3,15 +3,13 @@ agg_function_choices <- c("mean", "min", "max")
 
 # given a data frame, return the names of numeric columns
 cols_number <- function(df) {
-  df_select <- dplyr::select_if(df, ~is.numeric(.x) | is.integer(.x) ) 
-  
+  df_select <- dplyr::select(df, where(~is.numeric(.x) | is.integer(.x)) ) 
   names(df_select)
 }
 
 # given a data frame, return the names of string and factor columns
 cols_category <- function(df) {
-  df_select <- dplyr::select_if(df, ~is.character(.x) | is.factor(.x) ) 
-  
+  df_select <- dplyr::select(df, where(~is.character(.x) | is.factor(.x)) ) 
   names(df_select)
 }
 
